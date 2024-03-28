b248f9d5fb8204d36ea94662b956fcc6bb9e7034
Kirill <k890484878789@gmail.com>
2024-03-29 04:34:40 +0500

    add changesets

--- src/main/java/ru/oschepkov/library/entities/BookEntity.java
+++ src/main/java/ru/oschepkov/library/entities/BookEntity.java
@@ -3,37 +3,12 @@ package ru.oschepkov.library.entities;
 import java.util.Date;
 
 public class BookEntity {
+    private Long id;
     private String name;
-    private String author;
+    private PublisherEntity publisher;
     private Date publishDate;
-
-    public BookEntity(String name, String author, Date publishDate) {
-        this.name = name;
-        this.author = author;
-        this.publishDate = publishDate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public Date getPublishDate() {
-        return publishDate;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setPublishDate(Date publishDate) {
-        this.publishDate = publishDate;
-    }
 }
+
+//TODO: 1. Построение сущностей
+// 2. Построение changelog
+// 3. Заполнение changelog
--- src/main/java/ru/oschepkov/library/entities/PublisherEntity.java
+++ src/main/java/ru/oschepkov/library/entities/PublisherEntity.java
@@ -2,16 +2,4 @@ package ru.oschepkov.library.entities;
 
 public class PublisherEntity {
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public PublisherEntity(String name) {
-        this.name = name;
-    }
 }
