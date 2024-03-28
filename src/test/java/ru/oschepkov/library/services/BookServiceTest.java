305c1e3d5a934083ade8c19f517659f8de4e82a1
Kirill <k890484878789@gmail.com>
2024-03-28 17:20:24 +0500

    init project

---
+++ src/test/java/ru/oschepkov/library/services/BookServiceTest.java
@@ -0,0 +1,13 @@
+package ru.oschepkov.library.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+
+    @Test
+    void sum() {
+
+    }
+}
