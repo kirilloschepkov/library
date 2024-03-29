89cdc095f542c9d9135067b099ec891cd6c895b8
Kirill <k890484878789@gmail.com>
2024-03-29 13:41:28 +0500

    change changesets, add description entities

--- src/main/java/ru/oschepkov/library/entities/AuthorEntity.java
+++ src/main/java/ru/oschepkov/library/entities/AuthorEntity.java
@@ -1,8 +1,21 @@
 package ru.oschepkov.library.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authors", schema = "PUBLIC", catalog = "DB")
 public class AuthorEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "middle_name")
     private String middleName;
 }
